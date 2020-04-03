9b8cab6dbb6b6db54fb0bfc09d668dc68e5002d9
juicecultus <juicecultus@gmail.com>
2020-04-03 17:39:26 +0100

    add input history tracking react state with useState hook

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,26 @@ import { useState } from 'react';
 
 const InputElement = () => {
   const [inputText, setinputText] = useState('');
+  const [historyList, sethistoryList] = useState([]);
 
   return (
     <div>
       <input
-        onChange={e => {
+        onChange={(e) => {
           setinputText(e.target.value);
+          sethistoryList([...historyList, e.target.value]);
         }}
         placeholder='Enter some text'
       />
       <br />
       {inputText}
+      <hr />
+      <br />
+      <ul>
+        {historyList.map((rec) => {
+          return <div>{rec}</div>;
+        })}
+      </ul>
     </div>
   );
 };
